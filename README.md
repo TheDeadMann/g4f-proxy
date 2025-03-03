37d3e3923eec4aa21b74978f287a608fff307936
Jumanji <thedeadman951@gmail.com>
2025-03-03 03:54:24 +0200

    Finished the webserver (and uploaded it... 🙏)

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # g4f-proxy
 A webserver that takes a post request, passes the values to g4f and returns an answer
+
+run with `py server.py`
---
+++ server.py
@@ -0,0 +1,45 @@
+import os
+from g4f.client import Client
+from flask import Flask, jsonify, request
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+client = Client()
+
+port = int(os.getenv("PORT", 3001))
+
+@app.route('/', methods=['GET'])
+def getMain():
+    return 'g4f-proxy is running'
+
+@app.route('/', methods=['POST'])
+def postG4F():
+    if not request.is_json:
+        return jsonify({"error": "Request must be JSON"}), 400
+
+    data = request.get_json()
+
+    # Required attributes
+    required_fields = ["system", "user"]
+
+    # Check for missing fields
+    missing_fields = [field for field in required_fields if field not in data]
+
+    if missing_fields:
+        return jsonify({"error": f"Missing fields: {', '.join(missing_fields)}"}), 400
+
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": data['system']},
+            {"role": "user", "content": data['user']}
+        ],
+        web_search=False
+    )
+    
+    return response.choices[0].message.content
+
+if __name__ == '__main__':
+    app.run(debug=True, port=port)
